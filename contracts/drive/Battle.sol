3fcf549a79ebe07ff686f48094fcce5db3da6462
lambun <linbin.java@gmail.com>
2018-04-11 15:27:43 +0800

    modify Player.sol, add new functions.

--- contracts/drive/Battle.sol
+++ contracts/drive/Battle.sol
@@ -6,4 +6,5 @@ contract Battle {
     uint8 public constant MAX_TRY_COUNT = 10;
     uint8 private result;
 
+    
 }
--- contracts/drive/Player.sol
+++ contracts/drive/Player.sol
@@ -2,7 +2,41 @@ pragma solidity 0.4.21;
 
 
 contract Player {
+
+    event NewUser(User user);
+
+    mapping (address => User) private addressToUser;
     
-    enum Level {LV1, LV2}
+    struct User {
+        uint id;
+        string name;
+        uint32 level;
+        uint32 battleCount;
+        uint16 victoryCount;
+        uint16 defeatCount;
+    }
+
+    function getUserInfo(address _address) public view returns(
+        uint id,
+        string name,
+        uint32 level,
+        uint32 battleCount,
+        uint16 victoryCount,
+        uint16 defeatCount
+    ) {
+        User memory user = addressToUser[_address];
+        return (user.id, user.name, user.level, user.battleCount, user.victoryCount, user.defeatCount);
+    }
+
+    function createUser(address _address, string _name) public {
+        uint id = uint(keccak256(_address));
+        _createUser(_address, id, _name);
+    }
+
+    function _createUser(address _address, uint _id, string _name) internal {
+        User memory user = User(_id, _name, 0, 0, 0, 0);
+        NewUser(user);
+        addressToUser[_address] = user;
+    }
 
 }
